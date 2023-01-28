f57c1a93c864d86914321eafb77ae581692ed324
Sebastian Charmot <secharmot@davidson.edu>
2023-01-28 11:40:19 -0800

    fixed gif sorting

--- gif/create_gif.py
+++ gif/create_gif.py
@@ -1,17 +1,26 @@
 from PIL import Image
+import re
 import glob
  
 # Create the frames
 frames = []
 
 # sort by name so order is preserved 
-imgs = sorted(glob.glob("gif2/*.png"))
+# THIS METHOD DOES NOT WORK 
+# imgs = sorted(glob.glob("*.png"))
+
+imgs = sorted(glob.glob("*.png"))
+imgs.pop(0)
+imgs.sort(key=lambda f: int(re.sub('\D', '', f)))
+print(imgs)
 for i in imgs:
     new_frame = Image.open(i)
     frames.append(new_frame)
+
+
  
 # Save into a GIF file that loops forever
-frames[0].save('gif2/created_gif.gif', format='GIF',
+frames[0].save('created_gif_new.gif', format='GIF',
                append_images=frames[1:],
                save_all=True,
-               duration=300, loop=0)
+               duration=200, loop=0)
